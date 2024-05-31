99a4e79a19e26e9b6a6cc465472fa7767c603bf5
Jarreth Santos <santos.jarreth@gmail.com>
2024-05-31 09:39:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ 
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      
+      // Parse the JSON response
+      return response.json();
+    })
+    .then(data => {
+      // Pass the JSON object to renderBooks
+      renderBooks(data);
+    })
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +26,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
